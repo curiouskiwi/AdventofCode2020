6251cf3357a26a8c5227b1281296152271071b4e
Brenda Anderson <curiouskiwi@users.noreply.github.com>
2020-12-26 09:49:18 +1300

    Update day18.py
    got rid of the redundant parts from an earlier idea for solving

--- day18.py
+++ day18.py
@@ -1,68 +1,49 @@
-# Advent of Code - Day 18
-# @curiouskiwi / @gary_anderson
-# 25 Dec 2020  (This is part two ... completed part one on 18 Dec)
-
-
 def main():
-
     with open("data.txt", "r") as f:
         lines = f.readlines()
-    sum = 0
+    sum1 = 0
+    sum2 = 0
     for line in lines:
-        sum += parseeq(line)
-    print(sum)
+        sum1 += parseeq(line, 1)
+        sum2 += parseeq(line, 2)
+    print(f"Part 1: {sum1}\nPart 2: {sum2}")
 
 
-def parseeq (text):
+def parseeq (text, part):
     while True:
         start = text.rfind('(')
         if start == -1:
             break
         end = text[start:].find(')')
-        n = str(calc(text[start+1:start+end]))
+        n = str(calc(text[start+1:start+end],part))
         text = text[:start] + n + text[start+end+1:]
 
-    return calc(text)
+    return calc(text,part)
 
 
-def calc(eq):
-
-    parts = eq.split()
-    currentop = 'x'
-    a = 0
-    b = 0
+def calc(eq, part):
+    op = 'x'
     result = 0
-    done = 0
     interim = 1
-    for i, part in enumerate(parts):
+    for c in eq.split():
         try:
-            if currentop == 'x':
-                a = int(part)
-                result =a
+            if op == 'x':
+                result = int(c)
             else:
-                b = int(part)
-                if currentop == '+':
-                    result = calcit(result, b, currentop)
+                if op == '+':
+                    result += int(c)
                 else:
-                    interim *= result
-                    result = b
-                currentop = 'x'
+                    if part == 2:
+                        interim *= result
+                        result = int(c)
+                    else:
+                        result *= int(c)
+
+                op = 'x'
         except:
-            currentop = part
-                
+            op = c
+
     return result * interim
-
-
-# ['10', '+', '5', '+', '8', '-', '12', '*', '5', '-', '(3', '+', '4)']
-
-def calcit(a, b, op):
-    if op == '+':
-        return a+b
-    elif op == '*':
-        return a*b
-    else:
-        print("ERROR")
-        return 0
 
 
 main()
